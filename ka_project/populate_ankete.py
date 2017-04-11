2eb1425720163c12daed08ee48ee075116dad639
matonsjojc <matevz.podlipnik@gmail.com>
2017-04-11 16:45:51 +0200

    population script...

--- ka_project/populate_ankete.py
+++ ka_project/populate_ankete.py
@@ -6,3 +6,12 @@ import django
 django.setup()
 
 from ankete.models import Anketa, Question, Answer
+
+def populate():
+
+
+
+#start execution here
+if __name__ == '__main__':
+    print("Starting population script...")
+    populate()
